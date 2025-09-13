420eacf0c533c9397a4bb51e1150f3a8d8ef4de6
Ofentse Magongwa <magongwaotsile@gmail.com>
2025-09-13 17:34:34 +0200

    scripd

--- LibraryProject/relationship_app/models.py
+++ LibraryProject/relationship_app/models.py
@@ -25,4 +25,4 @@ class Librarian(models.Model):
     library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarian')
 
     def __str__(self):
-        return self.name
+        return self.name 
