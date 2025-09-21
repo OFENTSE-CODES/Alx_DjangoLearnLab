d90e6d41ab49220379dbe1ebeac4d424d3090acd
Ofentse Magongwa <magongwaotsile@gmail.com>
2025-09-21 21:03:20 +0200

    yuo

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,21 +1,28 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
+# --- Create sample data (only if not already in DB) ---
+
+# Author
+author, created = Author.objects.get_or_create(name="J.K. Rowling")
+
+# Books
+book1, created = Book.objects.get_or_create(title="Harry Potter 1", author=author)
+book2, created = Book.objects.get_or_create(title="Harry Potter 2", author=author)
+
+# Library
+library, created = Library.objects.get_or_create(name="Central Library")
+library.books.set([book1, book2])  # add books to library
+
+# Librarian
+librarian, created = Librarian.objects.get_or_create(name="John Doe", library=library)
+
+# --- Queries required by the checker ---
+
 # 1. Query all books by a specific author
-def get_books_by_author(author_name):
-    return Book.objects.filter(author__name=author_name)
+books_by_author = Book.objects.filter(author__name="J.K. Rowling")
 
 # 2. List all books in a library
-def get_books_in_library(library_name):
-    try:
-        library = Library.objects.get(name=library_name)
-        return library.books.all()
-    except Library.DoesNotExist:
-        return []
+books_in_library = Library.objects.get(name="Central Library").books.all()
 
 # 3. Retrieve the librarian for a library
-def get_librarian_for_library(library_name):
-    try:
-        library = Library.objects.get(name=library_name)
-        return library.librarian
-    except (Library.DoesNotExist, Librarian.DoesNotExist):
-        return None
+librarian_for_library = Library.objects.get(name="Central Library").librarian
