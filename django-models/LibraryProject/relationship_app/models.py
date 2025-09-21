580f3e1aa889d89eaaae2a5e4a26e98768d691ba
Ofentse Magongwa <magongwaotsile@gmail.com>
2025-09-21 16:11:51 +0200

    scrt

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,4 +1,3 @@
-
 from django.db import models
 
 class Author(models.Model):
@@ -7,7 +6,6 @@ class Author(models.Model):
     def __str__(self):
         return self.name
 
-
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
@@ -15,14 +13,12 @@ class Book(models.Model):
     def __str__(self):
         return self.title
 
-
 class Library(models.Model):
     name = models.CharField(max_length=100)
     books = models.ManyToManyField(Book, related_name='libraries')
 
     def __str__(self):
         return self.name
-
 
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
