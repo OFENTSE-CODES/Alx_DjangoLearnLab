ef44ad3d117f600b659d91c5b25599a82f7a6499
Ofentse Magongwa <magongwaotsile@gmail.com>
2025-09-28 21:07:45 +0200

    ilp

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -22,3 +22,15 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),  # Now api/books/ will route correctly
 ]
+
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
+
+    # Token authentication endpoint
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+]
