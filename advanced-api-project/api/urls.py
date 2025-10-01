e596864d6dc43b1f2ade76db43aa136496ccbd48
Ofentse Magongwa <magongwaotsile@gmail.com>
2025-10-01 13:47:01 +0200

    newww

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -10,4 +10,4 @@ urlpatterns = [
     path("books/create/", BookCreateView.as_view(), name="book-create"),
     path("books/update/<int:pk>/", BookUpdateView.as_view(), name="book-update"),
     path("books/delete/<int:pk>/", BookDeleteView.as_view(), name="book-delete"),
-] 
+]
