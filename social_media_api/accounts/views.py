8a3c18c4272f8e949ff024e9d1d6840ec1472a46
Ofentse Magongwa <magongwaotsile@gmail.com>
2025-10-07 22:21:52 +0200

    updated

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,42 +1,11 @@
-from django.shortcuts import render
-
-from rest_framework import generics, permissions
-from .models import CustomUser
-from .serializers import (
-    UserRegistrationSerializer, 
-    UserLoginSerializer, 
-    UserProfileSerializer
-)
+from rest_framework import generics, permissions, status
 from rest_framework.response import Response
-from rest_framework.views import APIView
-
-class RegisterView(generics.CreateAPIView):
-    queryset = CustomUser.objects.all()
-    serializer_class = UserRegistrationSerializer
-
-class LoginView(APIView):
-    def post(self, request):
-        serializer = UserLoginSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        return Response(serializer.validated_data)
-
-class ProfileView(generics.RetrieveUpdateAPIView):
-    queryset = CustomUser.objects.all()
-    serializer_class = UserProfileSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def get_object(self):
-        return self.request.user
-    
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status, permissions
 from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
 
 User = get_user_model()
 
-class FollowUserView(APIView):
+class FollowUserView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, user_id):
@@ -46,7 +15,8 @@ class FollowUserView(APIView):
         request.user.following.add(user_to_follow)
         return Response({"detail": f"You followed {user_to_follow.username}"}, status=status.HTTP_200_OK)
 
-class UnfollowUserView(APIView):
+
+class UnfollowUserView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, user_id):
