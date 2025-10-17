0e135d50cabd7849bad54b11554ba93e00fa4903
Ofentse Magongwa <magongwaotsile@gmail.com>
2025-10-17 23:02:56 +0200

    new urls

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -79,3 +79,16 @@ urlpatterns = [
     # Tag filtering
     path('tags/<slug:tag_slug>/', PostByTagListView.as_view(), name='posts-by-tag'),
 ]
+
+
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns += [
+    #  Auth views
+    path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
+]
