e0d4c63ff89da720e83a487a20bae950e5c2aa90
Ofentse Magongwa <magongwaotsile@gmail.com>
2025-10-17 17:35:55 +0200

    updated the views

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -2,58 +2,63 @@ from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.decorators import login_required  
 from .models import Post
 from .forms import PostForm
 
-# ListView to display all blog posts
+
+#  List all blog posts
 class PostListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'
-    context_object_name = 'posts'  # Context variable to use in the template
+    context_object_name = 'posts'
+    ordering = ['-created_at']  # Optional: order by latest first
 
-# DetailView to display a single blog post
+
+# Show details of a single blog post
 class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
-    context_object_name = 'post'  # Context variable to use in the template
+    context_object_name = 'post'
 
-# CreateView to create a new blog post (only for authenticated users)
+
+# Create a new post (only if logged in)
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
-    form_class = PostForm  # Custom form to handle the Post creation
+    form_class = PostForm
     template_name = 'blog/post_form.html'
+    success_url = reverse_lazy('post-list')
 
-    # Automatically set the author to the logged-in user
     def form_valid(self, form):
+        # Set the logged-in user as the author
         form.instance.author = self.request.user
         return super().form_valid(form)
 
-    success_url = reverse_lazy('post-list')  # Redirect after successful creation
 
-# UpdateView to edit an existing blog post (only for the post author)
+#  Update an existing post (only by the author)
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
-    form_class = PostForm  # Custom form to handle the Post update
+    form_class = PostForm
     template_name = 'blog/post_form.html'
+    success_url = reverse_lazy('post-list')
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
-
-    success_url = reverse_lazy('post-list')  # Redirect after successful update
 
     def test_func(self):
         post = self.get_object()
-        # Ensure that only the post author can update the post
+        # Only allow the author to edit the post
         return self.request.user == post.author
 
-# DeleteView to delete a blog post (only for the post author)
+
+#  Delete an existing post (only by the author)
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     template_name = 'blog/post_confirm_delete.html'
-    success_url = reverse_lazy('post-list')  # Redirect after successful deletion
+    success_url = reverse_lazy('post-list')
 
     def test_func(self):
         post = self.get_object()
-        # Ensure that only the post author can delete the post
+        # Only allow the author to delete the post
         return self.request.user == post.author
